c92c14fa9423fa08f9dda01b3127556d4e79ebf2
Thiago Capeleiro <thiagocapeleiro@gmail.com>
2014-12-09 17:13:42 -0200

    Added pagination feature

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,21 @@
+class CommentsController < ApplicationController
+	http_basic_authenticate_with name: "john", password: "secret", only: :destroy
+
+	def create
+		@post = Post.find(params[:post_id])
+			@comment = @post.comments.create(comments_params)
+		redirect_to post_path(@post)
+	end
+
+	def destroy
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.find(params[:id])
+		@comment.destroy
+		redirect_to post_path(@post)
+	end
+
+	private
+	def comments_params
+		params.require(:comment).permit(:commenter,:body)
+	end
+end
